ab1cc6eff0df71896c8b5ead27b2d5b83ef34fca
Jim Ladd <jladd@redhat.com>
2018-05-31 09:58:12 -0400

    Use plain @qe in place id

--- slackbot.py
+++ slackbot.py
@@ -85,8 +85,8 @@ def create_test_update():
     day_of_the_week = datetime.today().weekday()
     button_owner_map = {0: '<@U0EKSNVMM>', #jladd
                         1: '<@U033J8Q34>', #cwang
-                        2: '<@T026NJJ6Z>', #qe,
-                        3: '<@T026NJJ6Z>', #qe,
+                        2: '@qe',
+                        3: '@qe',
                         4: '<@U9T44HF35>'} #unlikelyzero
 
     button_owner_msg = ''
