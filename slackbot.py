ff421b84b0eb23a919f7d6bea457ad7b6d3e921b
Jim Ladd <jladd@redhat.com>
2018-07-11 12:12:26 -0400

    Improve parsing of job description

--- slackbot.py
+++ slackbot.py
@@ -21,11 +21,24 @@ slack = Slacker(slack_token)
 job_name = os.getenv('JOB_NAME')
 matrix_job = os.getenv('MATRIX_JOB', 'False')
 build_label = os.getenv('BUILD_LABEL')
-test_result_pattern = os.getenv('TEST_RESULT_PATTERN')
 
 show_button_owner = os.getenv('SHOW_BUTTON_OWNER')
 
+
 def get_test_results():
+    """
+    Returns either:
+    - (<number of currently failing tests>, <change in test failures>)
+    - (-1, 0) if last test run did not occur in past 24 hours, or
+    - (-2, 0) if test results were not found
+    """
+    def result_from_past_day(build):
+        timestamp = build.get_timestamp()
+        today = datetime.now(pytz.UTC)
+        if today - timestamp > timedelta(days=1):
+            return False
+        return True
+
     job = server.get_job(job_name)
     build_ids = sorted([id for id in job.get_build_ids()], reverse=True)
 
@@ -33,45 +46,58 @@ def get_test_results():
     found_first_result = False
     for id in build_ids:
         build = job.get_build(id)
+        runs = []
         if matrix_job.lower() == 'true':
-            matrix_runs = build.get_matrix_runs()
-            for run in matrix_runs:
-                desc = run.get_description()
-                if desc and build_label in desc:
-                    res = re.search(test_result_pattern, desc)
-                    if res and len(failure_history) < 2:
-                        total_failures = int(res.group(1)) + int(res.group(2))
-                        failure_history.append(total_failures)
+            runs = build.get_matrix_runs()
         else:
-            desc = build.get_description()
-            if desc and build_label in desc:
-                res = re.search(test_result_pattern, desc)
-                if res and len(failure_history) < 2:
-                    total_failures = int(res.group(1)) + int(res.group(2))
-                    failure_history.append(total_failures)
+            runs = [build]
 
-        if not found_first_result and len(failure_history) == 1:
-            found_first_result = True
-            timestamp = build.get_timestamp()
-            today = datetime.now(pytz.UTC)
-            if today - timestamp > timedelta(days=1):
-                return (-1, None)
+        for run in runs:
+            desc = run.get_description()
+            if not desc or build_label not in desc.strip('()'):
+                continue
+            desc = desc.lower()
+            results = {'passed': 0, 'failed': 0, 'error': 0}
+            for result in results:
+                res = re.search('(\d+) {}'.format(result), desc)
+                if not res:
+                    continue
+                results[result] = int(res.group(1))
+            if not any(results.values()):
+                continue
 
-        if len(failure_history) == 2:
-            break
+            if results['passed']:
+                if not found_first_result:
+                    if result_from_past_day(run):
+                        return (0, 0)
+                    return (-1, 0)
+                else:
+                    # Test was passing, now failing
+                    return (failure_history[0], failure_history[0])
 
-    if len(failure_history) < 2:
-        return (-2, None)
+            total_failures = results['failed'] + results['error']
+            failure_history.append(total_failures)
+            if not found_first_result and len(failure_history) == 1:
+                found_first_result = True
+                if not result_from_past_day(build):
+                    return (-1, None)
 
-    return failure_history[0], (failure_history[0] - failure_history[1])
+            if len(failure_history) == 2:
+                return failure_history[0], (failure_history[0] - failure_history[1])
+
+    return (-2, None)
 
 
 def post_slack_msg(text):
     slack.chat.post_message(slack_channel, text, icon_emoji=':ansible:')
-   
+
 
 def create_test_update():
     total_failures, change = get_test_results()
+
+    if total_failures == 0:
+        post_slack_msg(':green_ball: {0} is green! :green_ball:'.format(job_name))
+        return
 
     if total_failures == -1:
         post_slack_msg(':skull_and_crossbones: {0} failed to run :skull_and_crossbones:'.format(job_name))
@@ -93,7 +119,7 @@ def create_test_update():
         if change <= level:
             emoji = emoji_map[level]
             break
-    
+
     if change > 0:
         description = '{0} has {1} more failures'.format(job_name, change)
     elif change == 0:
